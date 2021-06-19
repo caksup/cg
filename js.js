7a1d77391bb64ba7b9d891035351ac405425b962
Cak Sup <32424007+caksup@users.noreply.github.com>
2021-06-19 13:43:37 +0700

    Create js.js

---
+++ js.js
@@ -0,0 +1,53 @@
+
+
+alert("A simple CSS generator to help you deal out CSS easily \n \n leave a comment for improvement");
+
+window.onload = function() {
+    var heart = document.getElementsByClassName("heart");
+    var classname = document.getElementsByClassName("tabitem");
+    var boxitem = document.getElementsByClassName("box");
+
+    var clickFunction = function(e) {
+        e.preventDefault();
+        var a = this.getElementsByTagName("a")[0];
+        var span = this.getElementsByTagName("span")[0];
+        var href = a.getAttribute("href").replace("#","");
+        for(var i=0;i<boxitem.length;i++){
+            boxitem[i].className =  boxitem[i].className.replace(/(?:^|\s)show(?!\S)/g, '');
+        }
+        document.getElementById(href).className += " show";
+        for(var i=0;i<classname.length;i++){
+            classname[i].className =  classname[i].className.replace(/(?:^|\s)active(?!\S)/g, '');
+        }
+        this.className += " active";
+        span.className += 'active';
+        var left = a.getBoundingClientRect().left;
+        var top = a.getBoundingClientRect().top;
+        var consx = (e.clientX - left);
+        var consy = (e.clientY - top);
+        span.style.top = consy+"px";
+        span.style.left = consx+"px";
+        span.className = 'clicked';
+        span.addEventListener('webkitAnimationEnd', function(event){
+            this.className = '';
+        }, false);  
+    };
+
+    for(var i=0;i<classname.length;i++){
+        classname[i].addEventListener('click', clickFunction, false);
+    }
+    for(var i=0;i<heart.length;i++){
+        heart[i].addEventListener('click', function(e) {
+            var classString = this.className, nameIndex = classString.indexOf("active");
+            if (nameIndex == -1) {
+                classString += ' ' + "active";
+            }
+            else {
+                classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+"active".length);
+            }
+            this.className = classString;
+
+        }, false);
+    }
+}
+
